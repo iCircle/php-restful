3271f74851c154849cd3f6477eb5ee5307b058e7
RaaghuKR <raghavendra@icircle.co.in>
2015-10-21 00:15:56 +0530

    Seed

---
+++ src/restful/RESTFul.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User: Raaghu
+ * Date: 20-10-2015
+ * Time: PM 10:51
+ */
+
+namespace icircle\restful;
+
+
+interface RESTFul{
+
+}
---
+++ src/restful/Route.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * User: Raaghu
+ * Date: 20-10-2015
+ * Time: PM 10:53
+ */
+
+namespace icircle\restful;
+
+
+class Route{
+    static function run(){
+
+
+
+
+    }
+}
